e77cc8e70a05242f90014d0ea031bc3a90fb9a44
spelinski <rohk@hotmail.com>
2017-11-17 15:22:54 -0600

    add some newlines back temp

--- stats.h
+++ stats.h
@@ -1,2 +1,47 @@
 #include <algorithm>
-template<class c>double sum(c a){return std::accumulate(a.begin(),a.end(),0.0);};template<class c>double mean(c a){return sum(a)/a.size();};template<class c>double median(c a){std::nth_element(a.begin(), a.begin()+(a.size()/2), a.end());return a[a.size()/2];};template<class c>double variance(c a){auto m=mean(a),s=0.0;for (auto& n: a){s+=(n-m)*(n-m);}return s/a.size();};template<class c>double stddev(c a){return sqrt(variance(a));};template<class c> c zscore(c a){auto r = a;auto m=mean(a),s=stddev(a),i=0.0;for(auto& n: a){r[i] = (n-m)/s;i++;}return r;};
+template<class c>
+double sum(c a)
+{
+	return std::accumulate(a.begin(),a.end(),0.0);
+};
+
+template<class c>
+double mean(c a)
+{
+	return sum(a)/a.size();
+};
+
+template<class c>
+double median(c a)
+{
+	std::nth_element(a.begin(), a.begin()+(a.size()/2), a.end());
+	return a[a.size()/2];
+};
+template<class c>
+double variance(c a)
+{
+	auto m=mean(a),s=0.0;
+	for (auto& n: a)
+	{
+		s += (n-m)*(n-m);
+	}
+	return s/a.size();
+};
+
+template<class c>
+double stddev(c a)
+{
+	return sqrt(variance(a));
+};
+
+template<class c> 
+c zscore(c a)
+{
+	auto r = a;
+	auto m=mean(a),s=stddev(a),i=0.0;
+	for(auto& n: a)
+	{
+		r[i] = (n-m)/s;i++;
+	}
+	return r;
+};
